e42191f4b58bdaf279bd8425d2873b63300f5595
Yago <yagogusmao1998@gmail.com>
2020-06-09 17:18:28 -0300

    upando heroku

--- src/service/ApiBase.js
+++ src/service/ApiBase.js
@@ -1,18 +1,17 @@
 import axios from "axios";
 import { getToken, getAuthenticate } from "./LocalAuth";
 
-const [localhost,server] = [
+const [localhost, server] = [
   "http://localhost:8080",
-  ""
+  "https://energyappie.herokuapp.com"
 ]
 
 const ApiBase = axios.create({
-  baseURL: localhost
+  baseURL: server
 });
 
 
 export const authorization = ApiBase.interceptors.request.use(async config => {
-  
   if (getAuthenticate()) {
     const token = getToken();
     config.headers.Authorization = `Bearer ${token}`;
